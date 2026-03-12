92178a64f0a99e4702475e48f18d853f9c02d8f4
DevBySuraj <97379244+DevBySuraj@users.noreply.github.com>
2026-03-12 18:22:42 +0530

    Implement numSubarrayBoundedMax function

---
+++ 24BCS11025_Suraj_KRG-616B/lab_mst/leetcode_795.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int n = nums.size();
+        int i =0;
+        int j =0;
+        int m= 0;
+        int count = 0;
+        while(j<n){
+            if(nums[j] > right){
+                m =0;
+                i = j+1;
+            }
+            else if(nums[j]  >=left && nums[j] <= right){
+                m = j-i+1;
+                count += m;
+            }
+            else if(nums[j] < left){
+                count +=m;
+            }
+
+            j++;
+        }
+        return count;
+    }
+};
